a69b1ebe2fd5734714f95f9d6b821f53631f66c0
Andrey Drobah <andrey.drobah.and@mail.ru>
2020-04-07 23:03:36 +0700

    Main Window layouts are changed and tab widget is added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,21 +3,39 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QLineEdit>
+#include <QTabWidget>
 
 
 
 MainWindow::MainWindow( QWidget * parent )
 	: QWidget( parent )
 {
-	QHBoxLayout * layout = new QHBoxLayout;
+	QHBoxLayout * lineEditLayout = new QHBoxLayout;
 	QLabel * label = new QLabel( tr( "Text" ) );
+
 	QLineEdit * lineEdit = new QLineEdit;
 	lineEdit->setFixedHeight( 50 );
-	layout->addWidget( label );
-	layout->addWidget( lineEdit );
-	layout->setSpacing( 20 );
-	setLayout( layout );
+	//lineEdit->setFont(
 
-	setFixedSize( 400, 150 );
+	lineEditLayout->addWidget( label );
+	lineEditLayout->addWidget( lineEdit );
+	lineEditLayout->setSpacing( 20 );
+
+	QTabWidget * window = new QTabWidget();
+	QWidget * page1 = new QWidget;
+	QWidget * page2 = new QWidget;
+	window->addTab( page1, tr( "Page 1" ) );
+	window->addTab( page2, tr( "Page 2" ) );
+	window->setTabShape( QTabWidget::Triangular );
+
+	QVBoxLayout * vLayout = new QVBoxLayout;
+	vLayout->setAlignment( Qt::AlignTop );
+	vLayout->addLayout( lineEditLayout );
+	vLayout->addWidget( window );
+
+	setLayout( vLayout );
+
+	setFixedWidth( 400 );
+	setMinimumHeight( 400 );
 }
 
